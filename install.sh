f5c81cd8581121723e259cbaec693e726b461b48
OG <github.com@landroverz.com>
2019-03-06 14:34:05 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,3 +23,5 @@ npm install --global yarn
 # Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
 rm -rf $HOME/.zshrc
 ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
+
+source .macos
