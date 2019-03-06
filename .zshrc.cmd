f9db22927d3e7ebb73c22bcf791f01094d0f2950
OG <github.com@landroverz.com>
2019-03-06 18:39:14 +0200

    added few commands

--- .zshrc.cmd
+++ .zshrc.cmd
@@ -1,3 +1,7 @@
+# This setting is for the new UTF-8 terminal support
+export LC_CTYPE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
 # Set default editor to vim
 export EDITOR='vim'
 
@@ -12,14 +16,10 @@ alias ~="cd ~"
 alias c='clear'
 alias path='echo -e ${PATH//:/\\n}'         # path:         Echo all executable Paths
 alias show_options='shopt'                  # Show_options: display bash options settings
+alias chdirs="find . -type d -exec chmod 755 {} \;"
+alias chfiles="find . -type f -exec chmod 644 {} \;"
 
-#   lr:  Full Recursive Directory Listing
-#   ------------------------------------------
-alias lr='ls -R | grep ":$" | sed -e '\''s/:$//'\'' -e '\''s/[^-][^\/]*\//--/g'\'' -e '\''s/^/   /'\'' -e '\''s/-/|/'\'' | less'
-
-
-#   extract:  Extract most know archives with one command
-#   ---------------------------------------------------------
+# extract: Extract most know archives with one command
 extract () {
     if [ -f $1 ] ; then
       case $1 in
@@ -43,46 +43,30 @@ extract () {
 
 
 
-#   ---------------------------
-#   PROCESS MANAGEMENT
-#   ---------------------------
+# PROCESS MANAGEMENT
 
-#   findPid: find out the pid of a specified process
-#   -----------------------------------------------------
-#       Note that the command name can be specified via a regex
-#       E.g. findPid '/d$/' finds pids of all processes with names ending in 'd'
-#       Without the 'sudo' it will only find processes of the current user
-#   -----------------------------------------------------
+# findPid: find out the pid of a specified process
 findPid () { lsof -t -c "$@" ; }
 
-#   memHogsTop, memHogsPs:  Find memory hogs
-#   -----------------------------------------------------
-    alias memHogsTop='top -l 1 -o rsize | head -20'
-    alias memHogsPs='ps wwaxm -o pid,stat,vsize,rss,time,command | head -10'
+# memHogsTop, memHogsPs:  Find memory hogs
+alias memHogsTop='top -l 1 -o rsize | head -20'
+alias memHogsPs='ps wwaxm -o pid,stat,vsize,rss,time,command | head -10'
 
-#   cpuHogs:  Find CPU hogs
-#   -----------------------------------------------------
+# cpuHogs:  Find CPU hogs
 alias cpu_hogs='ps wwaxr -o pid,stat,%cpu,time,command | head -10'
 
-#   topForever:  Continual 'top' listing (every 10 seconds)
-#   -----------------------------------------------------
+# topForever:  Continual 'top' listing (every 10 seconds)
 alias topForever='top -l 9999999 -s 10 -o cpu'
 
-#   ttop:  Recommended 'top' invocation to minimize resources
-#   ------------------------------------------------------------
-#       Taken from this macosxhints article
-#       http://www.macosxhints.com/article.php?story=20060816123853639
-#   ------------------------------------------------------------
+# ttop: Recommended 'top' invocation to minimize resources
+#  Taken from this macosxhints article
+#  http://www.macosxhints.com/article.php?story=20060816123853639
 alias ttop="top -R -F -s 10 -o rsize"
 
-#   my_ps: List processes owned by my user:
-#   ------------------------------------------------------------
-my_ps() { ps $@ -u $USER -o pid,%cpu,%mem,start,time,bsdtime,command ; }
+# psme: List processes owned by my user:
+psme() { ps $@ -u $USER -o pid,%cpu,%mem,start,time,bsdtime,command ; }
 
-
-#   ---------------------------
-#   NETWORKING
-#   ---------------------------
+# NETWORKING
 
 alias myip='curl ip.appspot.com'                    # myip:         Public facing IP Address
 alias netCons='lsof -i'                             # netCons:      Show all open TCP/IP sockets
@@ -93,47 +77,54 @@ alias lsockT='sudo /usr/sbin/lsof -nP | grep TCP'   # lsockT:       Display only
 alias ipInfo0='ipconfig getpacket en0'              # ipInfo0:      Get info on connections for en0
 alias ipInfo1='ipconfig getpacket en1'              # ipInfo1:      Get info on connections for en1
 alias openPorts='sudo lsof -i | grep LISTEN'        # openPorts:    All listening connections
-alias showBlocked='sudo ipfw list'                  # showBlocked:  All ipfw rules inc/ blocked IPs
 
-
-#   ii:  display useful host related informaton
-#   -------------------------------------------------------------------
+# ii: display useful host related informaton
 ii() {
-    echo -e "\nYou are logged on ${RED}$HOST"
-    echo -e "\nAdditionnal information:$NC " ; uname -a
-    echo -e "\n${RED}Users logged on:$NC " ; w -h
-    echo -e "\n${RED}Current date :$NC " ; date
-    echo -e "\n${RED}Machine stats :$NC " ; uptime
-    echo -e "\n${RED}Current network location :$NC " ; scselect
-    echo -e "\n${RED}Public facing IP Address :$NC " ;myip
-    #echo -e "\n${RED}DNS Configuration:$NC " ; scutil --dns
-    echo
+ echo -e "\nYou are logged on ${RED}$HOST"
+ echo -e "\nAdditionnal information:$NC " ; uname -a
+ echo -e "\n${RED}Users logged on:$NC " ; w -h
+ echo -e "\n${RED}Current date :$NC " ; date
+ echo -e "\n${RED}Machine stats :$NC " ; uptime
+ echo -e "\n${RED}Current network location :$NC " ; scselect
+ echo -e "\n${RED}Public facing IP Address :$NC " ;myip
+ #echo -e "\n${RED}DNS Configuration:$NC " ; scutil --dns
+ echo
 }
 
+# cleanupDS: Recursively delete .DS_Store files
+alias cleanDS="find . -type f -name '*.DS_Store' -ls -delete"
 
-#   cleanupDS:  Recursively delete .DS_Store files
-#   -------------------------------------------------------------------
-alias cleanupDS="find . -type f -name '*.DS_Store' -ls -delete"
+# cleanupLS: Clean up LaunchServices to remove duplicates in the "Open With" menu
+alias cleanLS="/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user && killall Finder"
 
-#   finderShowHidden:   Show hidden files in Finder
-#   finderHideHidden:   Hide hidden files in Finder
-#   -------------------------------------------------------------------
-alias finderShowHidden='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
-alias finderHideHidden='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
-
-#   cleanupLS:  Clean up LaunchServices to remove duplicates in the "Open With" menu
-#   -----------------------------------------------------------------------------------
-alias cleanupLS="/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user && killall Finder"
-
-#    screensaverDesktop: Run a screensaver on the Desktop
-#   -----------------------------------------------------------------------------------
+# screensaverDesktop: Run a screensaver on the Desktop
 alias screensaverDesktop='/System/Library/Frameworks/ScreenSaver.framework/Resources/ScreenSaverEngine.app/Contents/MacOS/ScreenSaverEngine -background'
-
-#   This setting is for the new UTF-8 terminal support
-export LC_CTYPE=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
-    
+  
 # short cuts
-alias editHosts='sudo edit /etc/hosts'
+alias hosts='sudo edit /etc/hosts'
+
+# System
 alias brewery='brew update && brew upgrade && brew cleanup'
+alias update='mas upgrade; brew cleanup; brew upgrade; brew update; brew cask cleanup; brew cu -a -y; composer global update; npm update -g; npm install npm@latest -g'
+alias show_files='defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder'
+alias hide_files='defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder'
+alias show_desktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
+alias hide_desktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
+alias enable_gate="sudo spctl --master-enable"
+alias disable_gate="sudo spctl --master-disable"
+
+# NPM
+alias nom='rm -rf node_modules/ && npm cache verify && npm install'
+
+# GitHub
+alias wip="git add .; git commit -m 'wip'"
+alias nah='git reset --hard; git clean -df'
+
+# Chrome
+alias kill_chrome="ps ux | grep '[C]hrome Helper --type=renderer' | grep -v extension-process | tr -s ' ' | cut -d ' ' -f2 | xargs kill"
+
+# Dummy
+alias shrug="printf '¯\_(ツ)_/¯' | pbcopy"
+alias flipt="printf '(╯°□°)╯︵ ┻━┻' | pbcopy"
+alias fight="printf '(ง'̀-'́)ง' | pbcopy"
 
