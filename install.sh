9a1d40c4501febc6c6f0a380a27898a462866f17
OG <github.com@landroverz.com>
2019-03-06 13:59:58 +0200

    initial

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+echo "Setting up your Mac..."
+
+# Check for Homebrew and install if we don't have it
+if test ! $(which brew); then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Update Homebrew
+brew update
+
+# Install all our dependencies with bundle (See Brewfile)
+brew tap homebrew/bundle
+brew bundle
+
+# Make ZSH the default shell environment
+chsh -s $(which zsh)
+
+# Install global NPM packages
+npm install --global yarn
+
+# Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
+rm -rf $HOME/.zshrc
+ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
